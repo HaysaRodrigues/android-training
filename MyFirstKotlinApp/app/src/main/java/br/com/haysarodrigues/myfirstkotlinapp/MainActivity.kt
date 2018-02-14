44467e9ff25aba6b5d5377210b28dcd044b137a7
Haysa <haysa.rodrigues@liferay.com>
2018-02-14 19:53:56 -0300

    Add sample kotlin made in class

---
+++ MyFirstKotlinApp/app/src/main/java/br/com/haysarodrigues/myfirstkotlinapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package br.com.haysarodrigues.myfirstkotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener{
+            toast("Message: ${edit.text}")
+        }
+    }
+}
